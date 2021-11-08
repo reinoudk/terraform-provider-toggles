a2351d570a2c284690a3d700fd035af6dec16667
Reinoud Kruithof <2184455+reinoudk@users.noreply.github.com>
2021-11-08 23:58:26 +0100

    Fix module import

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"terraform-provider-toggle/toggles"
+	"terraform-provider-toggles/toggles"
 )
 
 func main() {
